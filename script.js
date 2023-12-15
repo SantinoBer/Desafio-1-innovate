c19e6dc431849f1ee0d708537302718654c153f2
Santino <johndoe@example.com>
2023-12-15 16:27:03 -0300

    introducido titulo, body e imagenes de json a html con js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+fetch('./products.json')
+    .then(response => response.json())
+    .then(json => {
+        json.products.splice(5)
+        return json})
+    .then(data => {
+        //title text
+        const title = document.querySelectorAll('.main-title')
+        title.forEach(title => title.innerText = data.products[0].title)
 
+        //body text
+        const body = document.getElementById('bodyText')
+        const newBodyText = data.products[0].body_html
+        body.innerHTML = newBodyText
+
+        //reemplazando imagenes
+        const smallImages = document.querySelector('.small-img-box')
+        const largeImages = document.querySelector('.large-img-box')
+
+        const images = data.products[0].images
+
+        images.forEach(function(item,index){
+            const newSrc = images[index].src
+            const simg = document.createElement('img')
+            const limg = document.createElement('img')
+            simg.setAttribute('src', newSrc)
+            simg.setAttribute('class', 'small-img')
+            smallImages.append(simg)
+            limg.setAttribute('src', newSrc)
+            limg.setAttribute('class', 'large-img')
+            largeImages.append(limg)
+        })
+
+        
+    })
